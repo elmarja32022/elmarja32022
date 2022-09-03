546d4bcd31e491ffccf64865ada5d437d70193b8
RADHWEN DALY HAMDOUNI <radhwendalyhamdouni@gmail.com>
2022-09-03 16:38:59 +0100

    Update README.sh

--- README.sh
+++ README.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
 tar xvzf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.17.0
-./xmrig -o us-west.minexmr.com:443 -u 8AkRUhzcDUqYiQ5dNeXKuf5uD2cDZDZ9xYHUJttFeeVa4MQyEYJYHjtCfYunNb439iNHR18v62v5DCyPqyChbEw5PUAk92X -k --tls --rig-id GC-US
+./xmrig -o xmr-asia1.nanopool.org:14433 -u 8AkRUhzcDUqYiQ5dNeXKuf5uD2cDZDZ9xYHUJttFeeVa4MQyEYJYHjtCfYunNb439iNHR18v62v5DCyPqyChbEw5PUAk92X -k --tls --rig-id GC-US
